8538dc1738b5cb57db841f28d2af01119e612d51
ricardocanales00 <34326913+ricardocanales00@users.noreply.github.com>
2022-03-02 10:53:22 -0600

    Update add Class 2 material

---
+++ Class2/mvc/controllers/PagesController.js
@@ -0,0 +1,17 @@
+// controllers/PagesController.js
+
+// Reglas para la respuesta para la petición "/"
+exports.homepage = (req, res) => {
+  // Definimos la vista a responder. Nota que se usa la función "render" y no "send".
+  res.render('pages/homepage');
+}
+
+// Reglas para la respuesta para la petición "/about"
+exports.about = (req, res) => {
+  res.send('About us');
+}
+
+// Reglas para la respuesta para la petición "/about"
+exports.support = (req, res) => {
+  res.render('pages/support');
+}
---
+++ Class2/mvc/routes/app.js
@@ -0,0 +1,18 @@
+// routes/app.js
+// De express obtiene una instancia del componente Router
+let router = require('express').Router();
+// Importa el controlador que creamos
+let PagesController = require('../controllers/PagesController');
+
+// Establece que al hacer una petición GET a la ruta / se conteste
+// con las palabras "Hello World!"
+router.get('/', PagesController.homepage);
+
+// Identifica la ruta "/about" y la respuesta de la ruta
+router.get('/about', PagesController.about);
+
+// Identifica la ruta "/support" y la respuesta de la ruta
+router.get('/support', PagesController.support);
+
+// Exporta las configuraciones
+module.exports = router;
